d960dc2d7fe9f27140374e8deb7933c01fc83c4d
Katharina Hembach <katharinahemabch@gmail.com>
2018-05-28 14:52:35 +0200

    prepare BAM files and predict peaks with CLIPper
    Scripts to convert Ensembl BAM files to UCSC annotation and to run CLIPper.

--- clipper/convert_Ensembl_bam_to_UCSC.sh
+++ clipper/convert_Ensembl_bam_to_UCSC.sh
@@ -1,6 +1,7 @@
 ## This script converte a BAM file that was created with Ensembl annotation to UCSC annotation
 ## from https://www.biostars.org/p/10062/
 
+## For only the forward reads and the first 50nt
 ## First generate the header:
 # for f in $(ls ../STAR/*/*_1_Aligned.sortedByCoord.out.bam)
 # do
@@ -10,9 +11,26 @@
 ##And edit header.ucsc.sam to correct manually chrMT to chrM (sorry for no perl oneliner that does that!)
 # Then use reheader function
 
-for f in $(ls ../STAR/*/*_1_Aligned.sortedByCoord.out.bam)
+# for f in $(ls ../STAR/*/*_1_Aligned.sortedByCoord.out.bam)
+# do
+# 	samtools reheader ${f}_header.ucsc.sam $f > ${f%.bam}.ucsc.bam
+# done
+
+
+
+## ========== Deduplicated paired end reads ==========
+
+## First generate the header:
+# for f in $(ls ../BAM_deduplicated/*/*_deduplicated.bam)
+# do
+# 	samtools view -H $f | perl -lpe 's/SN:([0-9]+|[XY]|MT)\b/SN:chr$1/' > ${f}_header.ucsc.sam
+# done
+
+##And edit header.ucsc.sam to correct manually chrMT to chrM (sorry for no perl oneliner that does that!)
+# Then use reheader function
+
+for f in $(ls ../BAM_deduplicated/*/*_deduplicated.bam)
 do
 	samtools reheader ${f}_header.ucsc.sam $f > ${f%.bam}.ucsc.bam
 done
-
 
--- clipper/run_clipper.sh
+++ clipper/run_clipper.sh
@@ -1,15 +1,35 @@
 ## Run clipper peak calling on the ucsc converted BAM file (because the clipper annotation is based on UCSC chromosme names!!)
 ## Clipper only runs with python 2.7, so we first have to activate the conda environment
+# source activate py27
+
+# for f in $(ls ../STAR/*/*_1_Aligned.sortedByCoord.out.ucsc.bam)
+# do
+# 	echo ${f##*/}
+# 	base=${f##*/}
+# 	sample=${base%_1_Aligned.sortedByCoord.out.ucsc.bam}
+# 	echo clipper -b $f -s mm10 --processors=15
+# 	clipper -b $f -s mm10 --processors=15 
+# 	mv fitted_clusters ${sample}_clipper_peaks.bed
+# done
+
+# source deactivate
+ 
+
+
+
+## ========== Deduplicated paired end reads ==========
+
+
 source activate py27
-for f in $(ls ../STAR/*/*_1_Aligned.sortedByCoord.out.ucsc.bam)
+
+for f in ../BAM_deduplicated/SNS_70K/SNS_70K_deduplicated.ucsc.bam ../BAM_deduplicated/SNS_70K/SNS_70K_deduplicated.ucsc.bam 
 do
 	echo ${f##*/}
 	base=${f##*/}
-	sample=${base%_1_Aligned.sortedByCoord.out.ucsc.bam}
-	echo clipper -b $f -s mm10 --processors=15
-	clipper -b $f -s mm10 --processors=15 
-	mv fitted_clusters ${sample}_clipper_peaks.bed
+	sample=${base%_deduplicated.ucsc.bam}
+	echo clipper -b $f -s mm10 --processors=30
+	clipper -b $f -s mm10 --processors=30
+	mv fitted_clusters deduplicated_${sample}_clipper_peaks.bed
 done
-## we can deactivate the environment
+
 source deactivate
- 
