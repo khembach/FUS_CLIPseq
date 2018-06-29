38d61df900c867e8746ccd7b60dfad6a1d90a9b2
Katharina Hembach <katharinahemabch@gmail.com>
2018-06-29 14:30:28 +0200

    Run HOMER on the top residual peaks

--- scripts/find_homer_motifs.sh
+++ scripts/find_homer_motifs.sh
@@ -4,22 +4,35 @@
 
 # search for different motif sizes in one run
 
+# for f in SNS_70K
+#   do
+#   for a in exon three_prime_utr
+#   do
+#     echo HOMER: ${f} ${a}
+#     ~/software/homer/bin/findMotifs.pl ../analysis/deduplicated/peaks_fasta/${f}_clipper_peaks_${a}.fasta fasta ../analysis/deduplicated/HOMER/${f}_${a}_len4-8_bg_${a} -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_200000.fasta
+#   done
+# done
+
+# ## only the top 1000 peaks
+# for f in SNS_70K
+#   do
+#   for a in exon three_prime_utr
+#   do
+#     echo HOMER: ${f} ${a}
+#     FASTA="../analysis/deduplicated/peaks_fasta/${f}_clipper_top_*_peaks_${a}.fasta"
+#     ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/HOMER/${f}_top_peaks_${a}_len4-8_bg_${a} -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_200000.fasta
+#   done
+# done
+
+
+### Top 1000 peaks selected with max residual
 for f in SNS_70K
   do
   for a in exon three_prime_utr
   do
     echo HOMER: ${f} ${a}
-    ~/software/homer/bin/findMotifs.pl ../analysis/deduplicated/peaks_fasta/${f}_clipper_peaks_${a}.fasta fasta ../analysis/deduplicated/HOMER/${f}_${a}_len4-8_bg_${a} -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_200000.fasta
+    FASTA="../analysis/deduplicated/peaks_fasta/${f}_clipper_top_*_peaks_residual_${a}.fasta"
+    ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/HOMER/${f}_top_peaks_residual_${a}_len4-8_bg_${a} -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_200000.fasta
   done
 done
 
-## only the top 1000 peaks
-for f in SNS_70K
-  do
-  for a in exon three_prime_utr
-  do
-    echo HOMER: ${f} ${a}
-    FASTA="../analysis/deduplicated/peaks_fasta/${f}_clipper_top_*_peaks_${a}.fasta"
-    ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/HOMER/${f}_top_peaks_${a}_len4-8_bg_${a} -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_200000.fasta
-  done
-done
