576b0b9ddfd4f5598c11cf803e8ed316cdc27dc0
Katharina Hembach <katharinahemabch@gmail.com>
2018-07-05 15:24:04 +0200

    Find peak center and place window of +- 20nt around it

---
+++ scripts/peak_center_window.R
@@ -0,0 +1,110 @@
+## This script finds the center of a peak and extracts a window around the center.
+
+library(GenomicAlignments)
+library(rtracklayer)
+library(BSgenome.Mmusculus.UCSC.mm10)
+
+
+win_size <- 20
+
+
+base_dir <- "/Volumes/Shared/data/seq/sonu_CLIPseq/clip_March2018/"
+outdir <- "/Volumes/Shared/data/seq/sonu_CLIPseq/clip_March2018/analysis/deduplicated/peak_center_window"
+# GTF <- "/home/Shared/data/annotation/Mouse/Ensembl_GRCm38.90/gtf/Mus_musculus.GRCm38.90.gtf"
+GTF <- "/Users/katharina/PhD/data/annotation/Mouse/Mus_musculus.GRCm38.90.gtf"
+
+
+sample <- "SNS_70K"
+## deduplicated STAR alignments
+ga <- readGAlignmentPairs(file.path(base_dir,"BAM_deduplicated", sample, 
+                                 paste0(sample, "_deduplicated.bam")))
+## get coverage of all chromosomes
+# list with all chromosomes and the coverage
+cov <- coverage(ga)
+rm(ga)
+gc() ## garbage collection to foce R into freeing the memory space that aln took
+
+
+## For each peak, find the center of the peak and center a window around this position(s)
+clipper <- list()
+for (sample in c("HOMO_70K", "SNS_70K")){
+  clipper[[sample]] <- import(file.path(base_dir,"clipper",
+                                        paste0("deduplicated_", sample,"_clipper_peaks_top1000.bed")))
+}
+
+## how many peaks have more than one position with the max # reads? --> most peaks have only 1 position, max is 11
+max_peak_lengths <- sapply(which(cov[ clipper[[sample]] ] == max(cov[ clipper[[sample]] ])), length)
+# table(max_peak_lengths)
+
+## If a peak has more than one max position, we take all of them and center the window around it
+## How far are the positions apart?
+which(cov[ clipper[[sample]] ] == max(cov[ clipper[[sample]] ]))
+
+## range of positions with max value
+# a <- range(which(cov[ clipper[[sample]] ] == max(cov[ clipper[[sample]] ])))
+# table(a[,2] - a[,1] ) ## difference of positions 
+## --> they are no further than 34 nt apart --> window of length 41 convers all peaks!!
+
+## we take the median of all positions with the max coverage as the center
+clipper[[sample]]$peak_center <- start(clipper[[sample]]) + 
+  median(which(cov[ clipper[[sample]] ] == max(cov[ clipper[[sample]] ]))) -1
+
+## window 
+windows <- list()
+windows[[sample]] <- clipper[[sample]]
+start(windows[[sample]]) <- windows[[sample]]$peak_center - win_size
+end(windows[[sample]]) <- windows[[sample]]$peak_center + win_size
+
+
+## seperate the peaks according to annotation
+gtf <- import(GTF)
+anno <- split(gtf, mcols(gtf)$type)
+anno <- anno[c( "exon", "five_prime_utr", "three_prime_utr")]
+anno <- lapply(anno, unique)
+anno_intron_inv <- gtf[gtf$type %in% c( "exon", "five_prime_utr", "three_prime_utr")]
+
+
+peaks_anno_top <- list()
+peaks_anno_top[[sample]] <- lapply(anno, function(x)
+  subsetByOverlaps(windows[[sample]], x, type = "any") )
+names(peaks_anno_top[[sample]]) <- names(anno)
+peaks_anno_top[[sample]][["intron"]] <- subsetByOverlaps(windows[[sample]], anno_intron_inv, invert = TRUE)
+
+## write BED files with the peak windows
+for (a in c("exon", "three_prime_utr")){
+  export( peaks_anno_top[[sample]][[a]],
+          con = file.path(base_dir,"analysis", "deduplicated", "peak_center_window",
+                          paste0(sample, "_clipper_top_",
+                                 length(peaks_anno_top[[sample]][[a]]),
+                                 "_peaks_", a,"_window", 2*win_size, ".bed")),
+          format = "bed")
+}
+
+## write FASTA file
+genome <- BSgenome.Mmusculus.UCSC.mm10
+## conver the genome to Ensmble chromosome names
+seqnames(genome) <- gsub("chr", "", seqnames(genome) )
+
+for (a in c("exon", "three_prime_utr")){
+  export( getSeq(genome, peaks_anno_top[[sample]][[a]]),
+          con = file.path(base_dir,"analysis", "deduplicated", "peak_center_window",
+                          paste0(sample, "_clipper_top_",
+                                 length(peaks_anno_top[[sample]][[a]]),
+                                 "_peaks_", a,"_window", 2*win_size, ".fasta")),
+          format = "fasta")
+}
+
+## write fast in one line for RBPmotif
+for (a in c("exon", "three_prime_utr")){
+  seqs <- getSeq(genome, peaks_anno_top[[sample]][[a]])
+  seqs <- seqs[width(seqs)>=8]
+  writeXStringSet(x = , seqs,
+                  filepath = file.path(base_dir,"analysis", "deduplicated", "peak_center_window", 
+                                       paste0(sample, "_clipper_top_", 
+                                              length(seqs), 
+                                              "_peaks_", a, "_min8_window", 2*win_size, ".fasta")),
+                  width = max(width(seqs)))
+}
+
+
+
