a3b0fbda42af1cc0b28a1d00497318c68cd07322
Katharina Hembach <katharinahemabch@gmail.com>
2018-07-23 11:29:31 +0200

    Find HOMER motifs in top windows

--- scripts/find_homer_motifs.sh
+++ scripts/find_homer_motifs.sh
@@ -38,13 +38,37 @@
 
 
 ## Peak center window 40:
+# for f in SNS_70K
+#   do
+#   for a in exon three_prime_utr
+#   do
+#     echo HOMER: ${f} ${a}
+#     FASTA="../analysis/deduplicated/peak_center_window/${f}_clipper_top_*_peaks_${a}_window40.fasta"
+#     ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/HOMER/${f}_top_peaks_window40_${a}_len4-8_bg_${a} -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_200000.fasta
+#   done
+# done
+
+
+## window 40, bg 0 CLIP reads
+# for f in SNS_70K
+#   do
+#   for a in exon three_prime_utr
+#   do
+#     echo HOMER: ${f} ${a}
+#     FASTA="../analysis/deduplicated/peak_center_window/${f}_clipper_top_*_peaks_${a}_window40.fasta"
+#     ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/HOMER/${f}_top_peaks_window40_${a}_len4-8_bg_${a}_0reads_window40 -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_2e+05_0reads_window40.fasta
+#   done
+# done
+
+
+## window 40: top 2000 peaks, bg 0 CLIP reads
 for f in SNS_70K
   do
   for a in exon three_prime_utr
   do
     echo HOMER: ${f} ${a}
-    FASTA="../analysis/deduplicated/peak_center_window/${f}_clipper_top_*_peaks_${a}_window40.fasta"
-    ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/HOMER/${f}_top_peaks_window40_${a}_len4-8_bg_${a} -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_200000.fasta
+    FASTA="../analysis/deduplicated/peak_center_window/${f}_clipper_top2k_*_peaks_${a}_window40.fasta"
+    ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/HOMER/${f}_top2k_peaks_window40_${a}_len4-8_bg_${a}_0reads_window40 -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_2e+05_0reads_window40.fasta
   done
 done
 
