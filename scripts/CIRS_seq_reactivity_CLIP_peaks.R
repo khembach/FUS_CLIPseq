a99e216d3d3d526b501fe5c07ca231fe3517e154
Katharina Hembach <katharinahemabch@gmail.com>
2018-07-24 17:09:31 +0200

    plot mean CIRS reactivity seperately for windows with and without the HOMER motif

--- scripts/CIRS_seq_reactivity_CLIP_peaks.R
+++ scripts/CIRS_seq_reactivity_CLIP_peaks.R
@@ -40,7 +40,16 @@ seqlevels(reac) <- str_split(seqlevels(reac), "\\|", simplify = TRUE)[,1]
 #'
 #' @examples
 plot_mean_reactivity <- function(base_dir, npeaks, a, gtf, reac){ 
-
+  ## homer motif annotation in peak windows
+  ## The column names are really long and R parses them wrongly, so we just extract the columns that we are interested in
+  motif_loc <- read.table(file.path(base_dir, "analysis", "deduplicated", "HOMER", "motif_location", paste0("SNS_70K_clipper_top_", npeaks, "_peaks_", a, "_window40_annotatePeaks_motif1.txt")), header=TRUE, sep="\t")
+  motif_loc <- motif_loc[,c(1:5, 22)]
+  colnames(motif_loc)[1] <- unlist(strsplit(colnames(motif_loc)[1], split="\\."))[1]
+  colnames(motif_loc)[6] <- "motif"
+  
+  motif_loc$has_motif <- motif_loc$motif != ""  # TRUE if the motif was found in the peak
+  
+  ## peak windows
   bed <- import(file.path(base_dir, "analysis", "deduplicated", "peak_center_window", paste0("SNS_70K_clipper_top_", npeaks, "_peaks_", a, "_window40.bed")))
   
   ## transform to UCSC chromosome names, otherwise liftover doesn't work
@@ -48,6 +57,10 @@ plot_mean_reactivity <- function(base_dir, npeaks, a, gtf, reac){
   
   ## we use the package easyLift for lift over from mm10 to mm9
   bed <- easyLift::easyLiftOver(from = bed, map = "mm10_mm9")
+  
+  ## annotate if HOMER motif was found in the peak or not
+  motif_loc <- motif_loc[ match( bed$name, motif_loc$PeakID),]
+  bed$has_motif <- motif_loc$has_motif
 
   ## overlap the peaks with the list of transcripts with reactivity values
   tr_reac <- gtf[gtf$transcript_id %in% unique(reac$transcript_id) ]
@@ -65,6 +78,7 @@ plot_mean_reactivity <- function(base_dir, npeaks, a, gtf, reac){
   ex_reac_list <- split(ex_reac, names(ex_reac) )
   ## compute the start position of the window in the transcript
   tr_mapping <- mapToTranscripts(bed_olap, ex_reac_list)
+  tr_mapping$has_motif <- bed_olap[tr_mapping$xHits]$has_motif
   
  
   ### create transcript coverage object from  reactivity values
@@ -95,14 +109,30 @@ plot_mean_reactivity <- function(base_dir, npeaks, a, gtf, reac){
   }
   
   reac_mat_mean[which(reac_mat_mean<0.001)] <- 0
-  mean_reactivity <- colMeans(reac_mat_mean)
-  dat <- data.frame(position = 1:41, mean_reactivity = mean_reactivity)
   
-  p <- ggplot(dat, aes(x=position, y = mean_reactivity)) +
+  ## split the windows according to HOMER motif occurrence
+  reac_mat_mean  <- cbind(reac_mat_mean, as.integer( bed_olap[as.integer(rownames(reac_mat_mean))]$has_motif)  )
+  
+  mean_reactivity_motif <- colMeans(reac_mat_mean[reac_mat_mean[,42] == 1,1:41])  ## 16 windows
+  mean_reactivity_no_motif <- colMeans(reac_mat_mean[reac_mat_mean[,42] == 0,1:41])  ## 200
+  
+  dat <- data.frame(position = rep(1:41, 2), mean_reactivity = c(mean_reactivity_motif, mean_reactivity_no_motif),
+                    motif = rep(c("TRUE",  "FALSE"), each=41))
+  
+  p <- ggplot(dat, aes(x=position, y = mean_reactivity, col = motif)) +
     geom_line()+
     theme_bw()
-  ggsave(file.path(base_dir, "CIRS_reactivity", paste0("SNS_70K_clipper_top_", npeaks, "_peaks_", a, "_window40_CIRS_reactivity_mean_tr.png")), 
+  ggsave(file.path(base_dir, "CIRS_reactivity", paste0("SNS_70K_clipper_top_", npeaks, "_peaks_", a, "_window40_CIRS_reactivity_mean_tr_HOMERmotif.png")),
          plot=p, width=6, height=4)
+  
+  # mean_reactivity <- colMeans(reac_mat_mean)
+  # dat <- data.frame(position = 1:41, mean_reactivity = mean_reactivity)
+  # 
+  # p <- ggplot(dat, aes(x=position, y = mean_reactivity)) +
+  #   geom_line()+
+  #   theme_bw()
+  # ggsave(file.path(base_dir, "CIRS_reactivity", paste0("SNS_70K_clipper_top_", npeaks, "_peaks_", a, "_window40_CIRS_reactivity_mean_tr.png")), 
+  #        plot=p, width=6, height=4)
 }
 
 
