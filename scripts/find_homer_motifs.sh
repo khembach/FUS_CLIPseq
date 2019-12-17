13825cd8d82432d6ed334e36cdcd8594a9124b87
Katharina Hembach <3229704+khembach@users.noreply.github.com>
2019-12-17 15:17:59 +0100

    write fasta file with selected peaks and run HOMER motif analysis

--- scripts/find_homer_motifs.sh
+++ scripts/find_homer_motifs.sh
@@ -106,22 +106,58 @@
 
 
 ## No window, just the raw peak sequences
-for f in SNS_70K HOMO_70K 
+# for f in SNS_70K HOMO_70K 
+#   do
+#   for a in exon three_prime_utr
+#   do
+#     echo HOMER: ${f} ${a}
+#     FASTA="../analysis/omniCLIP/peaks_fasta/${f}_omniCLIP_top_*_peaks_${a}.fasta"
+#     ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/omniCLIP/HOMER/${f}_top_peaks_${a}_len4-8_bg_${a}_0reads_length_matched -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_2e+05_0reads_length_matched.fasta
+#   done
+# done
+
+# for f in HOMO_70K 
+#   do
+#   for a in intron
+#   do
+#     echo HOMER: ${f} ${a}
+#     FASTA="../analysis/omniCLIP/peaks_fasta/${f}_omniCLIP_top_*_peaks_${a}.fasta"
+#     ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/omniCLIP/HOMER/${f}_top_peaks_${a}_len4-8_bg_${a}_0reads_length_matched -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_2e+05_0reads_length_matched.fasta
+#   done
+# done
+
+
+
+## ==============================================================
+
+## Selected peaks (MA plot) with length match background sequences
+# for f in SNS total_cortex
+#   do
+#   for a in exon three_prime_utr five_prime_utr intron
+#   do
+#     echo HOMER: ${f} ${a}
+#     FASTA="../analysis/deduplicated/MA_plot_selection/peaks_fasta/${f}_selected_peaks_*_peaks_${a}.fasta"  
+#     ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/MA_plot_selection/HOMER/${f}_selected_peaks_${a}_len4-8_bg_${a}_0reads_length_matched -len 4,5,6,7,8 -rna -basic -fastaBg ../analysis/deduplicated/MA_plot_selection/peaks_fasta/${f}_bg_${a}_2e+05_0reads_length_matched.fasta
+#   done
+# done
+
+
+## Selected peaks with centered window of size 41
+# for f in SNS total_cortex
+#   do
+#   for a in exon three_prime_utr five_prime_utr intron
+#   do
+#     echo HOMER: ${f} ${a}
+#     FASTA="../analysis/deduplicated/MA_plot_selection/peaks_fasta/${f}_selected_peaks_*_peaks_${a}_window41.fasta"  
+#     ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/MA_plot_selection/HOMER/${f}_selected_peaks_${a}_len4-8_window41_bg_${a}_0reads_length_matched_window41 -len 4,5,6,7,8 -rna -basic -fastaBg ../analysis/deduplicated/MA_plot_selection/peaks_fasta/${f}_bg_${a}_2e+05_0reads_window41.fasta
+#   done
+# done
+
+## All peaks with centerd window, not seperated by location
+for f in SNS total_cortex
   do
-  for a in exon three_prime_utr
-  do
-    echo HOMER: ${f} ${a}
-    FASTA="../analysis/omniCLIP/peaks_fasta/${f}_omniCLIP_top_*_peaks_${a}.fasta"
-    ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/omniCLIP/HOMER/${f}_top_peaks_${a}_len4-8_bg_${a}_0reads_length_matched -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_2e+05_0reads_length_matched.fasta
-  done
+  echo HOMER: ${f}
+  FASTA="../analysis/deduplicated/MA_plot_selection/peaks_fasta/${f}_selected_peaks_*_peaks_window41.fasta"
+  ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/deduplicated/MA_plot_selection/HOMER/${f}_selected_peaks_all_len4-8_window41_bg_0reads_length_matched_window41 -len 4,5,6,7,8 -rna -basic -fastaBg ../analysis/deduplicated/MA_plot_selection/peaks_fasta/${f}_bg_all_peaks_2e+05_0reads_window41.fasta
 done
 
-for f in HOMO_70K 
-  do
-  for a in intron
-  do
-    echo HOMER: ${f} ${a}
-    FASTA="../analysis/omniCLIP/peaks_fasta/${f}_omniCLIP_top_*_peaks_${a}.fasta"
-    ~/software/homer/bin/findMotifs.pl ${FASTA} fasta ../analysis/omniCLIP/HOMER/${f}_top_peaks_${a}_len4-8_bg_${a}_0reads_length_matched -len 4,5,6,7,8 -rna -fastaBg ../analysis/deduplicated/peaks_fasta/${f}_bg_${a}_2e+05_0reads_length_matched.fasta
-  done
-done
